5f58a92616e4e38fa76b327bc6ddaa6d8bbccf18
themylogin <themylogin@gmail.com>
2014-01-24 05:06:22 +0700

    utils.tweet_with_lists

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from mock import Mock
+from testfixtures import replace
 import unittest
 
 from twitter_overkill.utils import *
@@ -18,3 +20,55 @@ class JoinListTestCase(unittest.TestCase):
 
     def test_three_items(self):
         self.assertEqual(join_list(["Мальчишки", "девчонки", "их родители"]), "Мальчишки, девчонки и их родители")
+
+
+class TweetWithListTestCase(unittest.TestCase):
+    @replace("twitter_overkill.utils.tweet_with_lists", Mock())
+    def test_proper_call(self, tweet_with_lists):
+        tweet_with_list("Беспонтово отдыхаем", "Пьём %s", ["пиво", "вино", "водку"])
+        tweet_with_lists.assert_called_with({(False,):  "Беспонтово отдыхаем",
+                                             (True,):   "Пьём %s"}, [["пиво", "вино", "водку"]])
+
+
+class TweetWithListsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = 8192
+
+    def test_one_list(self):
+        tweet = {
+            (False,):   "Беспонтово отдыхаем",
+            (True,):    "Пьём %s",
+        }
+        lists = [
+            ["пиво", "вино", "водку"]
+        ]
+        self.assertListEqual(tweet_with_lists(tweet, [[]]), ["Беспонтово отдыхаем"])
+        self.assertListEqual(tweet_with_lists(tweet, lists), ["Пьём пиво, вино и водку",
+                                                              "Пьём пиво и вино",
+                                                              "Пьём пиво",
+                                                              "Беспонтово отдыхаем"])
+
+    def test_two_lists(self):
+        tweet = {
+            (False, False): "Голодный день",
+            (False, True):  "Пьём %s",
+            (True,  False): "Едим %s",
+            (True,  True):  "Едим %s, и пьём %s",
+        }
+        lists = [
+            ["салатик", "борщ", "котлеты"],
+            ["пиво", "водку"],
+        ]
+        self.assertListEqual(tweet_with_lists(tweet, [[], []]), ["Голодный день"])
+        self.assertListEqual(tweet_with_lists(tweet, lists), ["Едим салатик, борщ и котлеты, и пьём пиво и водку",
+                                                              "Едим салатик и борщ, и пьём пиво и водку",
+                                                              "Едим салатик, борщ и котлеты, и пьём пиво",
+                                                              "Едим салатик, и пьём пиво и водку",
+                                                              "Едим салатик и борщ, и пьём пиво",
+                                                              "Едим салатик, борщ и котлеты",
+                                                              "Едим салатик, и пьём пиво",
+                                                              "Едим салатик и борщ",
+                                                              "Пьём пиво и водку",
+                                                              "Едим салатик",
+                                                              "Пьём пиво",
+                                                              "Голодный день"])
--- twitter_overkill/utils.py
+++ twitter_overkill/utils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, unicode_literals
+
+import itertools
+import numpy
+import operator
 
 
 def join_list(l, separator=", ", last_separator=" и "):
@@ -10,3 +14,73 @@ def join_list(l, separator=", ", last_separator=" и "):
         return l[0]
 
     return separator.join(l[:-1]) + last_separator + l[-1]
+
+
+def tweet_with_list(tweet_variant_wo_list, tweet_variant_w_list, list_, *args, **kwargs):
+    return tweet_with_lists({(False,): tweet_variant_wo_list, (True,): tweet_variant_w_list}, [list_], *args, **kwargs)
+
+
+def tweet_with_lists(tweet_variants, lists, *args, **kwargs):
+    tweets = []
+    """
+    Например, lists = [[1, 2, 3], [1, 2]]
+    Мы можем взять 3 элемента из первого списка и 2 из второго,
+        или 3 элемента из первого списка и 1 из второго,
+        или 3 элемента из первого списка и 0 из второго,
+        или 2 элемента из первого списка и 2 из второго,
+        или 2 элемента из первого списка и 1 из второго и т.п.
+    Это цикл по кортежам из количеств элементов, которые мы будем брать из списков.
+    Для вышеописанного примера lists_counts будет перебирать значения (3, 2), (3, 1), (3, 0), (2, 2), (2, 1), ...
+    """
+    for lists_counts in itertools.product(*[range(len(list_), -1, -1) for list_ in lists]):
+        """
+        Ключи словаря tweet_variants — кортежи булевых значений длиной, равной размеру lists.
+        i'ый элемент кортежа равен True, если текст твита предполагает, что i'ый список будет непустой.
+        Пример: {
+            (False, False): "Голодный день :(",
+            (False, True):  "Пью %s",
+            (True, False):  "Ем %s",
+            (True, True):   "Ем %s и пью %s"
+        }
+        """
+        tweet_variant = tweet_variants[tuple(map(lambda count: count > 0, lists_counts))]
+
+        lists_slices = map(lambda list_, count: list_[:count], lists, lists_counts)
+
+        tweet = tweet_variant % tuple(map(lambda list_: join_list(list_, *args, **kwargs), filter(None, lists_slices)))
+
+        tweets.append((tweet, lists_counts))
+
+    """
+    Всё хорошо, но твиты должны быть отсортированы по приоритету (максимально информативные — впереди), а у нас
+    получается так, что сначала мы урезаем первый список до нуля и наверняка получаем твит меньше 140 символов;
+    отправляем его без единого элемента первого списка, зато с двумя элементами второго. Поэтому отсортируем твиты,
+    сравнивая их следующим образом:
+    """
+    def cmp((tweet1, counts1), (tweet2, counts2)):
+        "1. Из твитов с разным количеством элементов списков более информативен тот, в котором элементов больше"
+        if sum(counts1) > sum(counts2):
+            return -1
+        elif sum(counts2) > sum(counts1):
+            return 1
+
+        """
+        2. Из твитов с одинаковой суммой количеств элементов список выбираем тот, где количества элементов более
+        сбалансированы (например, [1, 1] лучше, чем [2, 0])
+        """
+        std1 = numpy.std(counts1)
+        std2 = numpy.std(counts2)
+        if std1 < std2:
+            return -1
+        elif std2 < std1:
+            return 1
+
+        "И, наконец, наиболее информативен твит большей длины"
+        if len(tweet1) > len(tweet2):
+            return -1
+        elif len(tweet2) > len(tweet1):
+            return 1
+
+        return 0
+
+    return map(operator.itemgetter(0), sorted(tweets, cmp))
