c03022022d0d89136bd44f8d2c87655ea674be47
themylogin <themylogin@gmail.com>
2020-03-22 18:35:00 +0100

    Deleting tweets

--- twitter_overkill/__main__.py
+++ twitter_overkill/__main__.py
@@ -2,12 +2,13 @@ from flask_restful import Api
 
 from twitter_overkill.app import app
 from twitter_overkill.db import db
-from twitter_overkill.restful import Tweets
+from twitter_overkill.restful import TweetsResource, TweetResource
 
 
 if __name__ == "__main__":
     db.create_all()
 
     api = Api(app)
-    api.add_resource(Tweets, "/tweets")
+    api.add_resource(TweetsResource, "/tweets")
+    api.add_resource(TweetResource, "/tweet/<id>")
     app.run("0.0.0.0", 80)
--- twitter_overkill/restful.py
+++ twitter_overkill/restful.py
@@ -1,12 +1,17 @@
 from flask import request
 from flask_restful import abort, Resource, Api
+import twitter
 import uuid
 from voluptuous import Schema, Required, Any, MultipleInvalid
 
+from twitter_overkill.db import db
+from twitter_overkill.models import Tweet, TweetState
 from twitter_overkill.twitter import post_tweet
 
+__all__ = ["TweetsResource", "TweetResource"]
 
-class Tweets(Resource):
+
+class TweetsResource(Resource):
     def post(self):
         args = request.get_json(force=True, cache=False)
 
@@ -29,3 +34,21 @@ class Tweets(Resource):
         post_tweet.delay(id, args["auth"], args["text"] if isinstance(args["text"], list) else [args["text"]])
 
         return {"id": id}, 201
+
+
+class TweetResource(Resource):
+    def delete(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            abort(404)
+
+        if tweet.state != TweetState.POSTED:
+            abort(409)
+
+        api = twitter.Api(**tweet.auth)
+        api.DestroyStatus(tweet.state_data)
+
+        db.session.delete(tweet)
+        db.session.commit()
+
+        return {}, 204
